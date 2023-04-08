d860ac423e60aeb5b13464d747a533fbe15f62ac
Jose Vazquez <jose@teguz.com>
2023-04-08 14:44:12 -0500

    Initial Commit

---
+++ SwiftCharts/SwiftChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChartsApp.swift
+//  SwiftCharts
+//
+//  Created by Jose Vazquez on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
