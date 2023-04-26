3a6bbf6d4358ad4f7455c190f4a5b0d6b6f3ed15
Jose Vazquez <jose@teguz.com>
2023-04-26 07:40:10 -0500

    Fixed indentation

--- SwiftCharts/Pages/ManualBarMark1.swift
+++ SwiftCharts/Pages/ManualBarMark1.swift
@@ -11,11 +11,11 @@ import Charts
 struct ManualBarMark1: View {
     var body: some View {
         List {
-Chart {
-    BarMark(x: .value("Drink", "Mocha"),      y: .value("Serving", 1))
-    BarMark(x: .value("Drink", "Cappuccino"), y: .value("Serving", 3))
-    BarMark(x: .value("Drink", "Latte"),      y: .value("Serving", 2))
-}
+            Chart {
+                BarMark(x: .value("Drink", "Mocha"),      y: .value("Serving", 1))
+                BarMark(x: .value("Drink", "Cappuccino"), y: .value("Serving", 3))
+                BarMark(x: .value("Drink", "Latte"),      y: .value("Serving", 2))
+            }
             .frame(height: 300)
         }
     }
